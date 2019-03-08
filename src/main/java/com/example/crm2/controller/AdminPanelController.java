c767e64b867a73eab111d1713c19561d0ae3dc12
Damir <damir9715.com>
2019-03-08 23:32:40 +0600

    deploy on ec2 v1

--- src/main/java/com/example/crm2/controller/AdminPanelController.java
+++ src/main/java/com/example/crm2/controller/AdminPanelController.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.constraints.NotNull;
 import java.net.URI;
 import java.util.Optional;
 
